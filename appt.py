75b84ce38ac27429210173016c6831d13478b0e8
DiegoAlviarez <dilanalviarez@gmail.com>
2024-11-26 00:01:17 -0400

    Update appt.py

--- appt.py
+++ appt.py
@@ -23,8 +23,8 @@ def load_lottieurl(url):
 # Cargar datos
 @st.cache_data
 def load_data():
-    spain_data = pd.read_csv('https://raw.githubusercontent.com/AndersonP444/PROYECTO-SIC-JAKDG/main/valores_mercado_actualizados%20(3).csv')
-    bundesliga_data = pd.read_csv('https://raw.githubusercontent.com/AndersonP444/PROYECTO-SIC-JAKDG/main/valores_mercado_bundesliga_actualizado_v2.csv')
+    spain_data = pd.read_csv('https://raw.githubusercontent.com/AndersonP444/PROYECTO-SIC-JAKDG/main/CSV%20DESPUES%20DEL%20PROCESAMIENTO%20DE%20DATOS/valores_mercado_actualizados_con_estadisticas.csv')
+    bundesliga_data = pd.read_csv('https://raw.githubusercontent.com/AndersonP444/PROYECTO-SIC-JAKDG/main/CSV%20DESPUES%20DEL%20PROCESAMIENTO%20DE%20DATOS/valores_mercado_bundesliga_con_estadisticas.csv')
     return spain_data, bundesliga_data
 
 # Función para convertir valores de mercado
